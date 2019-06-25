786d0ca2a4ffcf0122918b573c87292a1360a986
José Sánchez-Gallego <gallegoj@uw.edu>
2019-06-24 23:46:21 -0700

    Add VimbaCPP and VimbaC libraries to compile options

--- python/basecam/examples/mantaCam/setup.py
+++ python/basecam/examples/mantaCam/setup.py
@@ -7,7 +7,7 @@
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 #
 # @Last modified by: José Sánchez-Gallego (gallegoj@uw.edu)
-# @Last modified time: 2019-06-24 12:31:50
+# @Last modified time: 2019-06-24 23:45:53
 
 # Setup file based on https://github.com/pybind/python_example
 
@@ -49,6 +49,7 @@ ext_modules = [
             get_pybind_include(),
             get_pybind_include(user=True)
         ],
+        libraries=['VimbaCPP', 'VimbaC'],
         language='c++'
     ),
 ]
