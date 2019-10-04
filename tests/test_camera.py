00cd9f925ae141b342777d6a29d48bf7ba5b4d16
José Sánchez-Gallego <gallegoj@uw.edu>
2019-10-03 21:17:50 -0700

    Some additional tests

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -7,13 +7,15 @@
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 #
 # @Last modified by: José Sánchez-Gallego (gallegoj@uw.edu)
-# @Last modified time: 2019-10-03 20:05:56
+# @Last modified time: 2019-10-03 21:09:20
 
+import astropy
 import astropy.io.fits
 import numpy
 import pytest
 
 from basecam.camera import VirtualCamera
+from basecam.utils import create_fits_image
 
 
 pytestmark = pytest.mark.asyncio
@@ -36,3 +38,15 @@ async def test_expose(camera):
     assert isinstance(header, astropy.io.fits.Header)
     assert header['EXPTIME'] == 1.0
     assert header['DATE-OBS'] == '2000-01-01T00:00:00.000'
+
+
+async def test_fits_no_obstime():
+
+    data = numpy.zeros((10, 10))
+
+    now = astropy.time.Time.now()
+
+    fits = create_fits_image(data, 1.0)
+    image_date = astropy.time.Time(fits[0].header['DATE-OBS'], format='isot')
+
+    assert (image_date - now).sec < 10
--- tests/test_camera_system.py
+++ tests/test_camera_system.py
@@ -7,14 +7,14 @@
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 #
 # @Last modified by: José Sánchez-Gallego (gallegoj@uw.edu)
-# @Last modified time: 2019-10-03 20:24:49
+# @Last modified time: 2019-10-03 21:16:48
 
 import asyncio
 
 import pytest
 
 from basecam.camera import VirtualCamera
-from basecam.exceptions import BasecamNotImplemented
+from basecam.exceptions import BasecamNotImplemented, BasecamUserWarning
 
 from .conftest import TEST_CONFIG_FILE, TestCameraSystem
 
@@ -76,3 +76,30 @@ async def test_get_cameras_not_implemented(camera_system, mocker):
 
     assert camera_system._camera_poller is not None
     assert camera_system._camera_poller.running is False
+
+
+async def test_config_bad_name(camera_system):
+
+    with pytest.warns(BasecamUserWarning):
+        data = camera_system.get_camera_config('BAD_CAMERA')
+
+    assert data['name'] == 'BAD_CAMERA'
+    assert data['uid'] is None
+
+
+async def test_no_config(camera_system):
+
+    camera_system.config = None
+
+    with pytest.warns(BasecamUserWarning):
+        data = camera_system.get_camera_config('test_camera')
+
+    assert data['name'] == 'test_camera'
+    assert data['uid'] is None
+
+
+async def test_config_from_uid(camera_system):
+
+    data = camera_system.get_camera_config(uid='DEV_12345')
+
+    assert data['name'] == 'test_camera'
