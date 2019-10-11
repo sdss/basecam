7e86ab5994ce22f503af57e86ac9e5e3a0e8fb82
José Sánchez-Gallego <gallegoj@uw.edu>
2019-10-10 19:30:27 -0700

    Add script to run interactive test actor

---
+++ tests/run_test_actor.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2019-10-10
+# @Filename: run_test_actor.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+import asyncio
+import os
+
+from basecam.actor import CameraActor
+
+from conftest import CameraSystemTester, VirtualCamera
+
+
+CONFIG_FILE = os.path.join(os.path.dirname(__file__), 'data/test_config.yaml')
+
+
+async def main(loop):
+
+    camera_system = CameraSystemTester(VirtualCamera, config=CONFIG_FILE).setup()
+    await camera_system.add_camera('test_camera')
+
+    camera_actor = await CameraActor.from_config(CONFIG_FILE, camera_system).run()
+    await camera_actor.server.server.serve_forever()
+
+
+if __name__ == '__main__':
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(loop))
