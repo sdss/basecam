bf7edcaa81f308d2118c5e34690ad21e995f3db1
José Sánchez-Gallego <gallegoj@uw.edu>
2019-10-10 20:28:50 -0700

    Add convenience function for command to get cameras to use

--- basecam/actor/commands.py
+++ basecam/actor/commands.py
@@ -6,8 +6,58 @@
 # @Filename: commands.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
-import clu
 from clu import command_parser as basecam_parser
+
+
+def get_cameras(command, cameras=None, check_cameras=True):
+    """A helper to determine what cameras to use.
+
+    Parameters
+    ----------
+    command
+        The command that wants to access the cameras.
+    cameras:
+        The output of the ``--cameras`` flag passed to the command.
+
+    Returns
+    -------
+    cameras : list of `.Camera` instances
+        A list of `.Camera` instances that match the input ``cameras`` or,
+        if ``cameras=None``, the default cameras. If ``cameras=None`` and
+        there are no default cameras defined; or if ``check_cameras=True`` and
+        any of the selected cameras is not connected, the command is failed
+        and returns `False`.
+
+    """
+
+    default = command.actor.default_cameras
+
+    if cameras is None:
+        if default is None or len(default) == 0:
+            command.set_status(command.flags.FAILED,
+                               text='no default camera set. User the --cameras flag.')
+            return False
+        else:
+            cameras = default
+
+    camera_instances = []
+    for camera in cameras:
+        camera_instance = command.actor.camera_system.get_camera(name=camera)
+        if camera_instance is False:
+            command.set_status(command.flags.FAILED,
+                               text=f'camera {camera} is not connected.')
+            return False
+        camera_instances.append(camera_instance)
+
+    if check_cameras:
+        for camera_instance in camera_instances:
+            if not camera_instance.connected:
+                command.set_status(command.flags.FAILED,
+                                   text=f'camera {camera_instance.name} '
+                                        'has not been initialised.')
+                return False
+
+    return camera_instances
 
 
 @basecam_parser.group()
--- tests/test_actor_camera.py
+++ tests/test_actor_camera.py
@@ -10,6 +10,8 @@ import asyncio
 
 import pytest
 
+from basecam.actor.commands import get_cameras
+
 
 pytestmark = pytest.mark.asyncio
 
@@ -24,5 +26,38 @@ async def test_camera_list(actor):
 
     assert command.is_done
     assert len(actor.mock_replies) == 2
-    print(actor.mock_replies)
     assert actor.mock_replies[1]['cameras'] == 'test_camera'
+
+
+async def test_get_cameras(command):
+
+    assert command.actor.default_cameras == ['test_camera']
+    assert command.status == command.flags.READY
+
+    assert get_cameras(command) == command.actor.camera_system.cameras
+
+
+async def test_get_cameras_no_default(command):
+
+    command.actor.set_default_cameras()
+    assert get_cameras(command) is False
+
+
+async def test_get_cameras_bad_default(command):
+
+    command.actor.set_default_cameras('bad_camera')
+    assert get_cameras(command) is False
+
+
+async def test_get_cameras_pass_cameras(command):
+
+    assert get_cameras(command, ['test_camera']) == command.actor.camera_system.cameras
+
+
+async def test_get_cameras_check(command):
+
+    command.actor.set_default_cameras('test_camera')
+    assert command.actor.camera_system.cameras[0].name == 'test_camera'
+    command.actor.camera_system.cameras[0].connected = False
+
+    assert get_cameras(command, check_cameras=True) is False
