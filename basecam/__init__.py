18b0e36d96e2ef67a4cec1fa0830135698a0b35e
José Sánchez-Gallego <gallegoj@uw.edu>
2019-12-25 00:13:42 -0800

    Some updates to the build system

--- basecam/__init__.py
+++ basecam/__init__.py
@@ -1,12 +1,18 @@
 # encoding: utf-8
 # flake8: noqa
 
+import pkg_resources
+
 from .camera import *
 from .events import *
 from .exceptions import *
 from .notifier import EventListener
+from .utils import get_config, get_logger
 
 
-__version__ = '0.1.0-alpha.0'
+try:
+    __version__ = pkg_resources.get_distribution('sdss-basecam').version
+except pkg_resources.DistributionNotFound:
+    __version__ = 'dev'
 
 NAME = 'basecam'
