c9aa70597bf6245e29d3279807bdf1498abb740f
José Sánchez-Gallego <gallegoj@uw.edu>
2020-01-19 20:46:11 -0800

    Fixed tests: command.status.is_done also includes failed states

--- tests/test_actor_commands.py
+++ tests/test_actor_commands.py
@@ -24,7 +24,7 @@ async def test_list(actor):
 
     await asyncio.sleep(1)
 
-    assert command.status.is_done
+    assert command.status == command.status.DONE
     assert len(actor.mock_replies) == 2
     assert actor.mock_replies[1]['cameras'] == ['test_camera']
 
@@ -87,15 +87,15 @@ async def test_set_default(actor):
     assert command_result.status.did_fail
 
     command_result = await actor.invoke_mock_command('set-default -f bad_camera')
-    assert command_result.status.is_done
+    assert command_result.status == command_result.status.DONE
     assert actor.default_cameras == ['bad_camera']
 
     command_result = await actor.invoke_mock_command('set-default -f sp1 sp2')
-    assert command_result.status.is_done
+    assert command_result.status == command_result.status.DONE
     assert actor.default_cameras == ['sp1', 'sp2']
 
     command_result = await actor.invoke_mock_command('set-default -f sp1,sp2 sp3')
-    assert command_result.status.is_done
+    assert command_result.status == command_result.status.DONE
     assert actor.default_cameras == ['sp1', 'sp2', 'sp3']
 
 
@@ -103,7 +103,7 @@ async def test_status(actor):
 
     command = await actor.invoke_mock_command('status')
 
-    assert command.status.is_done
+    assert command.status == command.status.DONE
     assert len(actor.mock_replies) == 3  # Running, status reply, and done reply.
     assert actor.mock_replies[1]['status'] == {'temperature': 25., 'cooler': 10.}
 
@@ -112,7 +112,7 @@ async def test_reconnect(actor):
 
     command = await actor.invoke_mock_command('reconnect')
 
-    assert command.status.is_done
+    assert command.status == command.status.DONE
 
 
 async def test_reconnect_disconnect_fails(actor):
@@ -121,7 +121,7 @@ async def test_reconnect_disconnect_fails(actor):
     command = await actor.invoke_mock_command('reconnect')
 
     assert 'failed to disconnect' in actor.mock_replies
-    assert command.status.is_done
+    assert command.status == command.status.DONE
 
 
 async def test_reconnect_connect_fails(actor):
