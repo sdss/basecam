3757d080d702cfd698a4be4dbc248bdcf93b0339
José Sánchez-Gallego <gallegoj@uw.edu>
2020-01-20 15:29:54 -0800

    Test cancel_task

---
+++ tests/test_utils.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2020-01-20
+# @Filename: test_utils.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+import asyncio
+
+import pytest
+
+from basecam.utils import cancel_task
+
+
+pytestmark = pytest.mark.asyncio
+
+
+async def test_cancel_task():
+
+    task = asyncio.create_task(asyncio.sleep(3))
+
+    assert await cancel_task(task) is None
+
+
+async def test_cancel_None():
+
+    assert await cancel_task(None) is None
