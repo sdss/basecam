0def52663b22d265e849af98c6e1014c18ea0ad2
José Sánchez-Gallego <gallegoj@uw.edu>
2020-12-07 19:29:40 -0800

    Add test for bad log path

--- tests/test_camera_system.py
+++ tests/test_camera_system.py
@@ -182,3 +182,17 @@ async def test_log_file(tmp_path):
 
     text = log_file.read_text()
     assert 'logging to ' + str(log_file) in text
+
+    # Remove the fh handler so that test_log_file_bad_path doesn't inherit it.
+    camera_system.logger.handlers.remove(camera_system.logger.fh)
+    camera_system.logger.fh = None
+
+
+async def test_log_file_bad_path():
+
+    log_file = '*InvalidPath*'
+
+    with pytest.warns(RuntimeWarning):
+        camera_system = CameraSystemTester(VirtualCamera, log_file=log_file)
+
+    assert camera_system.logger.fh is None
