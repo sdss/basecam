832e4a4ccb08943a399cbaf82c94371e428ba7c4
José Sánchez-Gallego <gallegoj@uw.edu>
2021-08-11 16:55:02 -0400

    Prevent circular import

--- basecam/__init__.py
+++ basecam/__init__.py
@@ -3,13 +3,14 @@
 
 from sdsstools import get_package_version
 
+
+NAME = "sdss-basecam"
+
+__version__ = get_package_version(__file__, "sdss-basecam") or "dev"
+
+
 from .camera import *
 from .events import *
 from .exceptions import *
 from .exposure import *
 from .notifier import *
-
-
-NAME = "sdss-basecam"
-
-__version__ = get_package_version(__file__, "sdss-basecam") or "dev"
