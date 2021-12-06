1c08f38c27bd3e4fbd3171af29cff0576eaa5cd2
José Sánchez-Gallego <gallegoj@uw.edu>
2021-12-05 20:53:12 -0700

    Scale with dtype name

--- basecam/models/fits.py
+++ basecam/models/fits.py
@@ -223,7 +223,9 @@ class Extension(object):
         # basic header (for example, if HDUClass is CompImageHDU this will add
         # the BITPIX keyword). Then append our header.
         hdu = HDUClass(data=data, header=None)
-        hdu.scale()
+
+        if data is not None:
+            hdu.scale(data.dtype.name)
 
         if self.header_model:
             hdu.header.extend(self.header_model.to_header(exposure, context=context))
